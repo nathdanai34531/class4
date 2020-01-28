dcdaafcfb8446b4836d39b37d396f1ccff4d795e
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-01-27 20:14:53 -0800

    class5

--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,41 +6,77 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
 
+    //State
     const [showMenu, updateShowMenu] = useState(true);
-    
+    const [isSmall, updateIsSmall] = useState(true);
 
-const handhamclick = () =>{
-    console.log('Youhitham') ;
 
-    let newShowMenu;
+    //Function /Methods
+    //Hamburger Click
+    const handhamclick = () => {
+        console.log('Youhitham');
 
-    if (showMenu === true) {
-        newShowMenu = false;
-    } else {
-        newShowMenu = true;
+        let newShowMenu;
+
+        if (showMenu === true) {
+            newShowMenu = false;
+        } else {
+            newShowMenu = true;
+        }
+
+
+        updateShowMenu(newShowMenu);
+    }
+
+    //Checking for Small view or Medium/Large
+    const handleIsSmall = () => {
+        console.log('How Wide Window: ', window.innerWidth, isSmall);
+
+        let newValue;
+
+        if (window.innerWidth > 599) {
+            console.log('greater than')
+            newValue = false;
+
+        } else {
+            console.log('less than')
+            newValue = true;
+
+        }
+        console.log('newValue', newValue)
+        updateIsSmall(newValue);
+
     }
 
 
-    updateShowMenu(newShowMenu);
-}
+    //useEffect
+    useEffect(() => {
+
+        window.addEventListener('resize', handleIsSmall)
+
+    }, []);
+
+
 
     return (
         <nav className='Nav'>
 
+            <button onClick={handleIsSmall}>Small me</button>
+
             <div className="hamburger" onClick={handhamclick}>
-                <FontAwesomeIcon icon={ faBars } />
+                <FontAwesomeIcon icon={faBars} />
 
             </div>
 
-{
-    showMenu &&
-            <div className="links">
-                <a href="#">Welcome</a> 
-                <a href="#">Services</a>
-                <a href="#">Contact</a>
-            </div>
+            {
+                showMenu &&
+                <div className="links">
+                    <a href="#">Welcome</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                </div>
 
-}
+            }
 
         </nav>
     )
