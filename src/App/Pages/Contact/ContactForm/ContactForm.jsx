77fa33d645ec0e8536f3d596f88bbac27104c52a
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-02-12 21:09:33 -0800

    Add contact form validation

--- src/App/Pages/Contact/ContactForm/ContactForm.jsx
+++ src/App/Pages/Contact/ContactForm/ContactForm.jsx
@@ -1,51 +1,141 @@
 import React, { useRef, useState } from 'react';
 import './ContactForm.scss';
-import {emailCheck} from '../../../common/utilities.js';
+import { emailCheck } from '../../../common/utilities.js';
+import classnames from 'classnames';
+import keycode from 'keycode';
+import API from '../../../common/API.js';
 
 const ContactForm = () => {
 
     // State
     const [emailIsValid, updateEmailIsValid] = useState(true);
+    const [formIsValid, updateFormIsValid] = useState(false);
+    const [errors, updateErrorsArray] = useState([]);
 
-    // Refs :: Giving virtual DOM access to the physicalDOM element on the page.
+
+    // Refs :: Giving virtual DOM access to the physical DOM element on the page.
     const emailRef = useRef();
     const messageRef = useRef();
 
 
-    const handleClick = () => {
+    const handleFormSubmit = () => {
         console.log('You clicked me');
+
+        let errorMessages = [];
+
+        //Validate the user fill in the form.
+        if (emailRef.current.value.length < 4) {
+            errorMessages.push({
+                message: 'You forgot to fill out the Email field',
+            })
+        }
+
+        if (!emailCheck(emailRef.current.value)) {
+            errorMessages.push({
+                message: 'The email you provided is invalid',
+            })
+        }
+
+        if (messageRef.current.value.length < 1) {
+            errorMessages.push({
+                message: 'You forgot to fill out the Message field',
+            })
+        }
+
+
+        updateErrorsArray(errorMessages);
+
+
+        //Keep track of errors - update the Dom with feedback if there's an error.
+        if (errorMessages.length > 0) {
+            updateFormIsValid(false);
+        } else {
+            updateFormIsValid(true);
+            //If all is sucessful - we want to post the data.
+            console.log('Posting the data');
+
+            const postData = {
+                email: emailRef.current.value,
+                message: messageRef.current.value,
+            }
+
+            API.post('sendmail', postData).then((result) => {
+                console.log('Posting the data', result);
+            });
+
+        }
     }
 
     const validateEmail = () => {
         const emailValue = emailRef.current.value;
-        console.log ('Validating email', emailValue)
+        console.log('Validating email', emailValue)
 
         if (emailValue.length > 3 && !emailCheck(emailValue)) {
-            updateEmailIsValid (false);
-        }else{
-            updateEmailIsValid (true);
+            updateEmailIsValid(false);
+        } else {
+            updateEmailIsValid(true);
         }
-        
+
 
 
     }
 
+    const displayErrors = () => {
+        return errors.map((error, idx) => {
+            return (
+                <li key={idx}>{error.message}</li>
+            );
+        });
+    }
+
+    // Handle keyboard events
+    const handleKeyDown = (event) => {
+        switch (keycode(event)) {
+            case 'space':
+            case 'enter':
+                handleFormSubmit();
+                break;
+            default:
+                return true;
+        }
+    }
+
+
+    // const theClassName = (formIsValid) ? 'ContactForm form-valid' : 'ContactForm form-invalid';
+
+    const theClassName = classnames({
+        'ContactForm': true,
+        'form-valid': formIsValid,
+        'form-invalid': !formIsValid,
+    });
 
     return (
-        <div className='ContactForm'>
+        <div className={theClassName}>
+
+            {
+                errors.length > 0 &&
+                <div className="error-message">
+                    Error message goes here.
+                <ul>
+                        {displayErrors()}
+                    </ul>
+                </div>
+
+            }
+
             <div className="form-group">
 
                 <div className="left">
                     <label htmlFor="email">Email</label>
                 </div>
                 <div className="right">
-                    <input 
-                    className={emailIsValid ? '':'invalid'}
-                    ref={emailRef} 
-                    name="email" 
-                    id="email" 
-                    placeholder='email.somedomain.com' 
-                    onChange={validateEmail}/>
+                    <input
+                        className={emailIsValid ? '' : 'invalid'}
+                        ref={emailRef}
+                        name="email"
+                        id="email"
+                        placeholder='email.somedomain.com'
+                        onChange={validateEmail} />
                 </div>
             </div>
             <div className="form-group">
@@ -60,7 +150,9 @@ const ContactForm = () => {
                 <div className="left" />
                 <div className="right">
                     <button
-                        onClick={handleClick}
+                        tab-index={0}
+                        onClick={handleFormSubmit}
+                        onKeyDown={handleKeyDown}
                     >Send Email</button>
                 </div>
             </div>
